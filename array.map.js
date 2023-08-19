115d1f982f8791beb7b08e5edc1c36f668e6d69b
ALTGAME <krgpsh13@gmail.com>
2023-08-19 22:55:48 +0530

    rtf

--- array.map.js
+++ array.map.js
@@ -1,12 +1,1 @@
-function replaceBlank(element) {
-    if (element === " ") {
-      return "empty string";
-    } else {
-      return element;
-    }
-  }
-
-  var array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-var newArray = array.map(replaceBlank);
-
-  
+console.log("HELLO");
