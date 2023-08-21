1c43e7ccddc22e666bf4d94178ba8fe8f2398da1
ALTGAME <krgpsh13@gmail.com>
2023-08-21 16:59:14 +0530

    message

--- web.js
+++ web.js
@@ -4,11 +4,17 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    const url= req.url;
+    if(url==='/'){
+        res.write('<html>');
+    res.write('<head><title>ENTER MESSAGE</title><head>');
+    res.write('<body><form action = "/message" method = "POST"><input type = "text" name =" "><button type = "submit"></button></form></body>');
+    res.write('</html>');
+    return res.end(); 
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.writeEarlyHints('<head><title>My First Page</title><head>');
+    res.write('<head><title>My First Page</title><head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
