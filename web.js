8f17b73e9e037df327e366321b52d3f1146402d3
ALTGAME <krgpsh13@gmail.com>
2023-08-21 21:51:13 +0530

    message 1

--- web.js
+++ web.js
@@ -1,16 +1,26 @@
 const http = require('http');
+const fs = require('fs');
+
 
 
 
 const server = http.createServer((req, res) => {
 
     const url= req.url;
-    if(url==='/'){
+    const method = req.method;
+    if(url ==='/'){
         res.write('<html>');
     res.write('<head><title>ENTER MESSAGE</title><head>');
     res.write('<body><form action = "/message" method = "POST"><input type = "text" name =" "><button type = "submit"></button></form></body>');
     res.write('</html>');
     return res.end(); 
+    }
+    if(url ==='/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -23,3 +33,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+
