f703623e7e54e531e5ed49a845ee3443825c333a
ALTGAME <krgpsh13@gmail.com>
2023-08-22 00:02:38 +0530

    Hello from EXPRESS

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 const http = require('http');
 
+const express =require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next)=>{
+
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next)=>{
+
+    console.log('In the another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+
+
+const server = http.createServer(app);
 
 
 server.listen(3000);
