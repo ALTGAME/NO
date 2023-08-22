587b1374c2e95c0a75d040443182ddc6100a89da
ALTGAME <krgpsh13@gmail.com>
2023-08-22 17:07:43 +0530

    FILTERING PATH

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express =require('express');
+
+const router = express.Router();
+
+router.get('/admin/add-product',(req, res, next)=>{
+
+    console.log('In the another middleware!');
+    res.send('<form action ="/add-product" method="POST"><input type ="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+
+router.post('/admin/add-product', (req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+module.exports = router;
